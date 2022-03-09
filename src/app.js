fe8d9602fdda41a23ff9b6666588da8897fa873d
Hazari <79424896+Hazaricoolbudy@users.noreply.github.com>
2022-03-09 20:17:28 +0530

    .nev file and jwt and bcrypt

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
+require('dotenv').config();
+const bcrybt=require("bcryptjs")
 const express = require("express");
 const path = require("path");
 const app = express();
 const hbs = require("hbs");
+const jwt=require('jsonwebtoken')
 
 require("./db/conn");
 
 const Register = require("./modules/register");
 const { json } = require("express");
+const async = require("hbs/lib/async");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -21,6 +25,8 @@ app.set("views", templatesPath);
 hbs.registerPartials(partialsPath);
 
 const port = process.env.PORT || 8000;
+
+console.log(process.env.SECRET_KEY);
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -44,6 +50,10 @@ app.post("/register", async (req, res) => {
         password: req.body.password,
         Confirmpassword: req.body.Confirmpassword,
       })
+      // concept of middle ware
+        const token=await registerStudent.generateAuthToken()
+        
+
       const result=await registerStudent.save()
       res.status(200).render("index");
     } else {
@@ -56,8 +66,58 @@ app.post("/register", async (req, res) => {
 });
 app.get("/login", (req, res) => {
   res.render("login");
-  res.send("hello from the other side");
+  
 });
+
+// login Check 
+
+app.post("/login", async(req, res) => {
+    try {
+        const email=req.body.email;
+        const password=req.body.password;
+      const userEmail= await Register.findOne({email:email})
+      const isMatch= await bcrybt.compare(password,userEmail.password)
+      const token=await userEmail.generateAuthToken()
+      console.log("token of login is"+token)
+
+      if(isMatch){
+          res.status(201).render("index")
+      }
+      else{
+          res.send("Invelid login details")
+      }  
+      
+
+    } catch (error) {
+        res.status(400).send("Invelid login detailso");
+        
+    }
+    
+  });
+
+
+
+// const securePassword= async(password)=>{
+//     const passwordHash=await bcrybt.hash(password,12);
+//     const passwordMatch=await bcrybt.compare(password,passwordHash);
+//     console.log(passwordMatch);
+
+// }
+// securePassword("hazari@123");
+
+// const jwt=require('jsonwebtoken')
+// const createToken= async ()=>{
+    // jwt.sign({_id:""},"sz")
+// }
+
+
+
+// createToken()
+
+
+
+
+
 
 app.listen(port, () => {
   console.log("connection sucessful");
@@ -66,3 +126,6 @@ app.listen(port, () => {
 // app.listen(port,()=>{
 
 // })
+
+
+
--- src/modules/register.js
+++ src/modules/register.js
@@ -1,4 +1,8 @@
+const bcrypt = require('bcryptjs/dist/bcrypt');
+const async = require('hbs/lib/async');
 const mongoose = require('mongoose');
+const jwt=require('jsonwebtoken');
+const res = require('express/lib/response');
 
 const employeeSchema = new mongoose.Schema({
     firstname: {
@@ -38,8 +42,39 @@ const employeeSchema = new mongoose.Schema({
     Confirmpassword:{
         type:String,
         required:true
-    }
+    },
+    tokens:[{
+        token:{
+            type:String,
+            required:true
+        }
+    }]
 })
+// generating  of token 
+
+employeeSchema.methods.generateAuthToken= async function(){
+    try {
+        const token=jwt.sign({_id:this._id.toString()},process.env.SECRET_KEY);
+        this.tokens=this.tokens.concat({token:token});
+        await  this.save()
+        return token;
+        
+        
+    } catch (error) {
+        res.send("occured error is"+error)
+    }
+}
+
+employeeSchema.pre("save", async function(next){
+    // const passwordHash=await bcrypt.hash(password,12);
+    if(this.isModified("password")){
+    this.password= await bcrypt.hash(this.password,12)
+     this.Confirmpassword= await bcrypt.hash(this.Confirmpassword,12)
+    }
+     next()
+}
+)
+
 
 const Register=new mongoose.model("Register",employeeSchema)
 
